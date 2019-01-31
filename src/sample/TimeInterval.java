4d95b5ca3350f9a17af7dc91edbba72b15890769
BT_1N3_22 <cwu8959@bths.edu>
2019-01-31 11:10:31 -0500

    update

---
+++ src/sample/TimeInterval.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TimeInterval {
+    Date start, end;
+
+    public TimeInterval(Date start, Date end) {
+        this.start = start;
+        this.end = end;
+    }
+}
