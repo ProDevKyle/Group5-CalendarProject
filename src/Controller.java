d88351e0955cecb6d439dd0978d5bd52fe2147fe
KarwaiKang <Bicycle#Gate$Shorten+8>
2019-02-01 10:42:19 -0500

    Remove sample package

---
+++ src/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Controller {
+    private int[] DAYS_IN_MONTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    @FXML
+    public void initialize() {
+
+    }
+}
