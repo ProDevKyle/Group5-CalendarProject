b0ab7f175ff0d60867563d2f0e2436b9f214bfd3
BT_1N3_22 <cwu8959@bths.edu>
2019-02-01 10:37:07 -0500

    update

--- src/sample/Event.java
+++ src/sample/Event.java
@@ -3,9 +3,11 @@ package sample;
 public class Event {
     TimeInterval TimeInterval;
     ArrayList<String> tags;
+    String name;
 
-    public Event(TimeInterval time, ArrayList<String> tags) {
+    public Event(TimeInterval time, ArrayList<String> tags, String name) {
         this.TimeInterval = time;
         this.tags = tags;
+        this.name = name;
     }
 }
