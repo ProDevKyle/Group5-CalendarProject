88f2d24cedf862df7006796e453efe8df4077d0a
KarwaiKang <Bicycle#Gate$Shorten+8>
2019-02-01 11:38:05 -0500

    Tweak css

--- src/Controller.java
+++ src/Controller.java
@@ -1,10 +1,26 @@
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import java.util.Calendar;
 
 public class Controller {
     private int[] DAYS_IN_MONTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     @FXML
-    public void initialize() {
+    private Label datetime;
 
+    @FXML
+    public void initialize() {
+        new AnimationTimer() {
+            Long time = System.currentTimeMillis();
+            @Override
+            public void handle(long now) {
+                if (System.currentTimeMillis() - time > 1000) {
+                    time = System.currentTimeMillis();
+                    datetime.setText(Calendar.getInstance().getTime().toString());
+                }
+            }
+        }.start();
     }
 }
---
+++ src/Date.java
@@ -0,0 +1,51 @@
+public class Date {
+    private int month, day, hour, minute, second;
+
+    public Date(int month, int day, int hour, int minute, int second) {
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+}
